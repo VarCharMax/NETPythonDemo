a4f206e92f1a2167bb9dcc0f13a8eceb610b4233
Rohan Parkes <rparkes@email.com>
2026-01-12 16:58:38 +1100

    Added commandline parsing library.

--- NETPython/Program.cs
+++ NETPython/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Python.Runtime;
+using System.CommandLine;
+using System.CommandLine.Parsing;
 
 namespace NETPython
 {
@@ -9,29 +11,72 @@ namespace NETPython
       string pathToVirtualEnv = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Scripts", ".venv");
       string message;
 
-      using PythonInitialiser pyInit = new();
-      if ((message = pyInit.InitialisePy(pathToVirtualEnv)) != "")
+      Option<string> outputpathOption = new("--output", "-o")
       {
-        Console.WriteLine(message);
+        Description = "An option whose argument is parsed as a string",
+        DefaultValueFactory = parseResult => @"C:\tmp",
+      };
+
+      Option<int> countOption = new("--delay", "-d")
+      {
+        Description = "An option whose argument is parsed as an int",
+        DefaultValueFactory = parseResult => 5,
+      };
+
+      RootCommand rootCommand = new();
+        
+      rootCommand.Options.Add(outputpathOption);
+      rootCommand.Options.Add(countOption);
+
+      ParseResult parseResult = rootCommand.Parse(args);
+      if (parseResult.Errors.Count == 0)
+      {
+        string outputPath = parseResult.GetValue(outputpathOption)!;
+        int numCount = parseResult.GetValue(countOption)!;
+
+        if (Path.Exists(outputPath) == false)
+        {
+          return;        
+        }
+
+        using PythonInitialiser pyInit = new();
+        if ((message = pyInit.InitialisePy(pathToVirtualEnv)) != "")
+        {
+          Console.WriteLine(message);
+          return;
+        }
+
+        using (Py.GIL())
+        {
+          
+
+          try
+          {
+            dynamic module = Py.Import("rw_visual");
+            module.create_plot(numCount, outputPath);
+          }
+          catch (PythonException pex)
+          {
+            Console.WriteLine(pex.Format());
+          }
+          catch (Exception ex)
+          {
+            Console.WriteLine(ex.Message);
+          }
+        }
+
         return;
       }
 
-      using (Py.GIL())
+      foreach (ParseError parseError in parseResult.Errors)
       {
-        try
-        {
-          dynamic module = Py.Import("rw_visual");
-          module.create_plot(5, @"C:\tmp");
-        }
-        catch(PythonException pex)
-        {
-          Console.WriteLine(pex.Format());
-        }
-        catch (Exception ex)
-        {
-          Console.WriteLine(ex.Message);
-        }
+        Console.Error.WriteLine(parseError.Message);
       }
+      return;
+
+      
+
+     
 
       // Console.ReadKey();
     }
